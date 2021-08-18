e075fcbaffc215e7fd155c49241f73bf1e0b1bc1
nati-m <it.natnael.mekonnen@gmail.com>
2021-08-18 20:23:45 +0300

    flutter client setup

---
+++ client/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:client/injectable.dart';
+import 'package:client/presentation/core/app_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:injectable/injectable.dart';
+
+Future main() async {
+  await dotenv.load(fileName: "../.env");
+  configureInjection(Environment.prod);
+  runApp(AppWidget());
+}
---
+++ client/lib/presentation/core/app_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
