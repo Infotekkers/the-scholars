d8e73c08fb0c8b653df3572df8757c7da202a7d5
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2021-09-04 18:21:42 +0300

    Completed Application Model

--- client/lib/domain/application/application.dart
+++ client/lib/domain/application/application.dart
@@ -1,11 +1,35 @@
+import 'package:client/domain/application/value_objects.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'application.freezed.dart';
 
 @freezed
 abstract class Application with _$Application {
   // Constructor
-  const factory Application() = _Application;
+  const factory Application({
+    required SchoolTranscript schoolTranscript,
+    required MainEssay mainEssay,
+    required ExtraEssay extraEssay,
+    required DepartmentSelection departmentSelection,
+    required ExtraCertification extraCertification,
+    required ProficencyTest proficencyTest,
+    required RecomendationLetter recomendationLetter,
+    required MilitaryFamilyStatus militaryFamilyStatus,
+    required UniversityFamilyStatus universityFamilyStatus,
+  }) = _Application;
 
-// Intial creator
-  factory Application.initial() => Application();
+  // Inital Application Creator
+  factory Application.initial() => Application(
+        schoolTranscript: SchoolTranscript(schoolTranscriptPath: ""),
+        mainEssay: MainEssay(mainEssayPath: ""),
+        extraEssay: ExtraEssay(extraEssayPath: ""),
+        departmentSelection: DepartmentSelection(departmentSelection: []),
+        extraCertification: ExtraCertification(extraCertificationPath: ""),
+        proficencyTest: ProficencyTest(
+          proficiencyUrl: "",
+        ),
+        recomendationLetter: RecomendationLetter(recomendationLetterPath: ""),
+        militaryFamilyStatus: MilitaryFamilyStatus(militaryFamilyStatus: ""),
+        universityFamilyStatus:
+            UniversityFamilyStatus(universityFamilyStatus: ""),
+      );
 }
--- client/lib/domain/application/application.freezed.dart
+++ client/lib/domain/application/application.freezed.dart
@@ -16,8 +16,27 @@ final _privateConstructorUsedError = UnsupportedError(
 class _$ApplicationTearOff {
   const _$ApplicationTearOff();
 
-  _Application call() {
-    return const _Application();
+  _Application call(
+      {required SchoolTranscript schoolTranscript,
+      required MainEssay mainEssay,
+      required ExtraEssay extraEssay,
+      required DepartmentSelection departmentSelection,
+      required ExtraCertification extraCertification,
+      required ProficencyTest proficencyTest,
+      required RecomendationLetter recomendationLetter,
+      required MilitaryFamilyStatus militaryFamilyStatus,
+      required UniversityFamilyStatus universityFamilyStatus}) {
+    return _Application(
+      schoolTranscript: schoolTranscript,
+      mainEssay: mainEssay,
+      extraEssay: extraEssay,
+      departmentSelection: departmentSelection,
+      extraCertification: extraCertification,
+      proficencyTest: proficencyTest,
+      recomendationLetter: recomendationLetter,
+      militaryFamilyStatus: militaryFamilyStatus,
+      universityFamilyStatus: universityFamilyStatus,
+    );
   }
 }
 
@@ -25,13 +44,42 @@ class _$ApplicationTearOff {
 const $Application = _$ApplicationTearOff();
 
 /// @nodoc
-mixin _$Application {}
+mixin _$Application {
+  SchoolTranscript get schoolTranscript => throw _privateConstructorUsedError;
+  MainEssay get mainEssay => throw _privateConstructorUsedError;
+  ExtraEssay get extraEssay => throw _privateConstructorUsedError;
+  DepartmentSelection get departmentSelection =>
+      throw _privateConstructorUsedError;
+  ExtraCertification get extraCertification =>
+      throw _privateConstructorUsedError;
+  ProficencyTest get proficencyTest => throw _privateConstructorUsedError;
+  RecomendationLetter get recomendationLetter =>
+      throw _privateConstructorUsedError;
+  MilitaryFamilyStatus get militaryFamilyStatus =>
+      throw _privateConstructorUsedError;
+  UniversityFamilyStatus get universityFamilyStatus =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ApplicationCopyWith<Application> get copyWith =>
+      throw _privateConstructorUsedError;
+}
 
 /// @nodoc
 abstract class $ApplicationCopyWith<$Res> {
   factory $ApplicationCopyWith(
           Application value, $Res Function(Application) then) =
       _$ApplicationCopyWithImpl<$Res>;
+  $Res call(
+      {SchoolTranscript schoolTranscript,
+      MainEssay mainEssay,
+      ExtraEssay extraEssay,
+      DepartmentSelection departmentSelection,
+      ExtraCertification extraCertification,
+      ProficencyTest proficencyTest,
+      RecomendationLetter recomendationLetter,
+      MilitaryFamilyStatus militaryFamilyStatus,
+      UniversityFamilyStatus universityFamilyStatus});
 }
 
 /// @nodoc
@@ -41,13 +89,77 @@ class _$ApplicationCopyWithImpl<$Res> implements $ApplicationCopyWith<$Res> {
   final Application _value;
   // ignore: unused_field
   final $Res Function(Application) _then;
+
+  @override
+  $Res call({
+    Object? schoolTranscript = freezed,
+    Object? mainEssay = freezed,
+    Object? extraEssay = freezed,
+    Object? departmentSelection = freezed,
+    Object? extraCertification = freezed,
+    Object? proficencyTest = freezed,
+    Object? recomendationLetter = freezed,
+    Object? militaryFamilyStatus = freezed,
+    Object? universityFamilyStatus = freezed,
+  }) {
+    return _then(_value.copyWith(
+      schoolTranscript: schoolTranscript == freezed
+          ? _value.schoolTranscript
+          : schoolTranscript // ignore: cast_nullable_to_non_nullable
+              as SchoolTranscript,
+      mainEssay: mainEssay == freezed
+          ? _value.mainEssay
+          : mainEssay // ignore: cast_nullable_to_non_nullable
+              as MainEssay,
+      extraEssay: extraEssay == freezed
+          ? _value.extraEssay
+          : extraEssay // ignore: cast_nullable_to_non_nullable
+              as ExtraEssay,
+      departmentSelection: departmentSelection == freezed
+          ? _value.departmentSelection
+          : departmentSelection // ignore: cast_nullable_to_non_nullable
+              as DepartmentSelection,
+      extraCertification: extraCertification == freezed
+          ? _value.extraCertification
+          : extraCertification // ignore: cast_nullable_to_non_nullable
+              as ExtraCertification,
+      proficencyTest: proficencyTest == freezed
+          ? _value.proficencyTest
+          : proficencyTest // ignore: cast_nullable_to_non_nullable
+              as ProficencyTest,
+      recomendationLetter: recomendationLetter == freezed
+          ? _value.recomendationLetter
+          : recomendationLetter // ignore: cast_nullable_to_non_nullable
+              as RecomendationLetter,
+      militaryFamilyStatus: militaryFamilyStatus == freezed
+          ? _value.militaryFamilyStatus
+          : militaryFamilyStatus // ignore: cast_nullable_to_non_nullable
+              as MilitaryFamilyStatus,
+      universityFamilyStatus: universityFamilyStatus == freezed
+          ? _value.universityFamilyStatus
+          : universityFamilyStatus // ignore: cast_nullable_to_non_nullable
+              as UniversityFamilyStatus,
+    ));
+  }
 }
 
 /// @nodoc
-abstract class _$ApplicationCopyWith<$Res> {
+abstract class _$ApplicationCopyWith<$Res>
+    implements $ApplicationCopyWith<$Res> {
   factory _$ApplicationCopyWith(
           _Application value, $Res Function(_Application) then) =
       __$ApplicationCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {SchoolTranscript schoolTranscript,
+      MainEssay mainEssay,
+      ExtraEssay extraEssay,
+      DepartmentSelection departmentSelection,
+      ExtraCertification extraCertification,
+      ProficencyTest proficencyTest,
+      RecomendationLetter recomendationLetter,
+      MilitaryFamilyStatus militaryFamilyStatus,
+      UniversityFamilyStatus universityFamilyStatus});
 }
 
 /// @nodoc
@@ -59,27 +171,187 @@ class __$ApplicationCopyWithImpl<$Res> extends _$ApplicationCopyWithImpl<$Res>
 
   @override
   _Application get _value => super._value as _Application;
+
+  @override
+  $Res call({
+    Object? schoolTranscript = freezed,
+    Object? mainEssay = freezed,
+    Object? extraEssay = freezed,
+    Object? departmentSelection = freezed,
+    Object? extraCertification = freezed,
+    Object? proficencyTest = freezed,
+    Object? recomendationLetter = freezed,
+    Object? militaryFamilyStatus = freezed,
+    Object? universityFamilyStatus = freezed,
+  }) {
+    return _then(_Application(
+      schoolTranscript: schoolTranscript == freezed
+          ? _value.schoolTranscript
+          : schoolTranscript // ignore: cast_nullable_to_non_nullable
+              as SchoolTranscript,
+      mainEssay: mainEssay == freezed
+          ? _value.mainEssay
+          : mainEssay // ignore: cast_nullable_to_non_nullable
+              as MainEssay,
+      extraEssay: extraEssay == freezed
+          ? _value.extraEssay
+          : extraEssay // ignore: cast_nullable_to_non_nullable
+              as ExtraEssay,
+      departmentSelection: departmentSelection == freezed
+          ? _value.departmentSelection
+          : departmentSelection // ignore: cast_nullable_to_non_nullable
+              as DepartmentSelection,
+      extraCertification: extraCertification == freezed
+          ? _value.extraCertification
+          : extraCertification // ignore: cast_nullable_to_non_nullable
+              as ExtraCertification,
+      proficencyTest: proficencyTest == freezed
+          ? _value.proficencyTest
+          : proficencyTest // ignore: cast_nullable_to_non_nullable
+              as ProficencyTest,
+      recomendationLetter: recomendationLetter == freezed
+          ? _value.recomendationLetter
+          : recomendationLetter // ignore: cast_nullable_to_non_nullable
+              as RecomendationLetter,
+      militaryFamilyStatus: militaryFamilyStatus == freezed
+          ? _value.militaryFamilyStatus
+          : militaryFamilyStatus // ignore: cast_nullable_to_non_nullable
+              as MilitaryFamilyStatus,
+      universityFamilyStatus: universityFamilyStatus == freezed
+          ? _value.universityFamilyStatus
+          : universityFamilyStatus // ignore: cast_nullable_to_non_nullable
+              as UniversityFamilyStatus,
+    ));
+  }
 }
 
 /// @nodoc
 
 class _$_Application implements _Application {
-  const _$_Application();
+  const _$_Application(
+      {required this.schoolTranscript,
+      required this.mainEssay,
+      required this.extraEssay,
+      required this.departmentSelection,
+      required this.extraCertification,
+      required this.proficencyTest,
+      required this.recomendationLetter,
+      required this.militaryFamilyStatus,
+      required this.universityFamilyStatus});
+
+  @override
+  final SchoolTranscript schoolTranscript;
+  @override
+  final MainEssay mainEssay;
+  @override
+  final ExtraEssay extraEssay;
+  @override
+  final DepartmentSelection departmentSelection;
+  @override
+  final ExtraCertification extraCertification;
+  @override
+  final ProficencyTest proficencyTest;
+  @override
+  final RecomendationLetter recomendationLetter;
+  @override
+  final MilitaryFamilyStatus militaryFamilyStatus;
+  @override
+  final UniversityFamilyStatus universityFamilyStatus;
 
   @override
   String toString() {
-    return 'Application()';
+    return 'Application(schoolTranscript: $schoolTranscript, mainEssay: $mainEssay, extraEssay: $extraEssay, departmentSelection: $departmentSelection, extraCertification: $extraCertification, proficencyTest: $proficencyTest, recomendationLetter: $recomendationLetter, militaryFamilyStatus: $militaryFamilyStatus, universityFamilyStatus: $universityFamilyStatus)';
   }
 
   @override
   bool operator ==(dynamic other) {
-    return identical(this, other) || (other is _Application);
+    return identical(this, other) ||
+        (other is _Application &&
+            (identical(other.schoolTranscript, schoolTranscript) ||
+                const DeepCollectionEquality()
+                    .equals(other.schoolTranscript, schoolTranscript)) &&
+            (identical(other.mainEssay, mainEssay) ||
+                const DeepCollectionEquality()
+                    .equals(other.mainEssay, mainEssay)) &&
+            (identical(other.extraEssay, extraEssay) ||
+                const DeepCollectionEquality()
+                    .equals(other.extraEssay, extraEssay)) &&
+            (identical(other.departmentSelection, departmentSelection) ||
+                const DeepCollectionEquality()
+                    .equals(other.departmentSelection, departmentSelection)) &&
+            (identical(other.extraCertification, extraCertification) ||
+                const DeepCollectionEquality()
+                    .equals(other.extraCertification, extraCertification)) &&
+            (identical(other.proficencyTest, proficencyTest) ||
+                const DeepCollectionEquality()
+                    .equals(other.proficencyTest, proficencyTest)) &&
+            (identical(other.recomendationLetter, recomendationLetter) ||
+                const DeepCollectionEquality()
+                    .equals(other.recomendationLetter, recomendationLetter)) &&
+            (identical(other.militaryFamilyStatus, militaryFamilyStatus) ||
+                const DeepCollectionEquality().equals(
+                    other.militaryFamilyStatus, militaryFamilyStatus)) &&
+            (identical(other.universityFamilyStatus, universityFamilyStatus) ||
+                const DeepCollectionEquality().equals(
+                    other.universityFamilyStatus, universityFamilyStatus)));
   }
 
   @override
-  int get hashCode => runtimeType.hashCode;
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(schoolTranscript) ^
+      const DeepCollectionEquality().hash(mainEssay) ^
+      const DeepCollectionEquality().hash(extraEssay) ^
+      const DeepCollectionEquality().hash(departmentSelection) ^
+      const DeepCollectionEquality().hash(extraCertification) ^
+      const DeepCollectionEquality().hash(proficencyTest) ^
+      const DeepCollectionEquality().hash(recomendationLetter) ^
+      const DeepCollectionEquality().hash(militaryFamilyStatus) ^
+      const DeepCollectionEquality().hash(universityFamilyStatus);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ApplicationCopyWith<_Application> get copyWith =>
+      __$ApplicationCopyWithImpl<_Application>(this, _$identity);
 }
 
 abstract class _Application implements Application {
-  const factory _Application() = _$_Application;
+  const factory _Application(
+      {required SchoolTranscript schoolTranscript,
+      required MainEssay mainEssay,
+      required ExtraEssay extraEssay,
+      required DepartmentSelection departmentSelection,
+      required ExtraCertification extraCertification,
+      required ProficencyTest proficencyTest,
+      required RecomendationLetter recomendationLetter,
+      required MilitaryFamilyStatus militaryFamilyStatus,
+      required UniversityFamilyStatus universityFamilyStatus}) = _$_Application;
+
+  @override
+  SchoolTranscript get schoolTranscript => throw _privateConstructorUsedError;
+  @override
+  MainEssay get mainEssay => throw _privateConstructorUsedError;
+  @override
+  ExtraEssay get extraEssay => throw _privateConstructorUsedError;
+  @override
+  DepartmentSelection get departmentSelection =>
+      throw _privateConstructorUsedError;
+  @override
+  ExtraCertification get extraCertification =>
+      throw _privateConstructorUsedError;
+  @override
+  ProficencyTest get proficencyTest => throw _privateConstructorUsedError;
+  @override
+  RecomendationLetter get recomendationLetter =>
+      throw _privateConstructorUsedError;
+  @override
+  MilitaryFamilyStatus get militaryFamilyStatus =>
+      throw _privateConstructorUsedError;
+  @override
+  UniversityFamilyStatus get universityFamilyStatus =>
+      throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ApplicationCopyWith<_Application> get copyWith =>
+      throw _privateConstructorUsedError;
 }
