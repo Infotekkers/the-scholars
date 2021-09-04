86083497bb7d9c2a2d76c728d1afd71db47f67bb
michael alemayehu <mikealexiv565@gmail.com>
2021-09-04 15:55:54 +0300

    register form bloc state and event added

--- client/lib/application/auth/sign_in_form/sign_in_form_event.dart
+++ client/lib/application/auth/sign_in_form/sign_in_form_event.dart
@@ -2,7 +2,11 @@ part of 'sign_in_form_bloc.dart';
 
 @freezed
 abstract class SignInFormEvent with _$SignInFormEvent {
+  const factory SignInFormEvent.nameChanged(String nameStr) = NameChanged;
   const factory SignInFormEvent.emailChanged(String emailStr) = EmailChanged;
-  const factory SignInFormEvent.passwordChanged(String passwordStr) = PasswordChanged;
+  const factory SignInFormEvent.passwordChanged(String passwordStr) =
+      PasswordChanged;
+  const factory SignInFormEvent.roleChanged(String roleStr) = RoleChanged;
+  const factory SignInFormEvent.registerPressed() = RegistrationClicked;
   const factory SignInFormEvent.signInPressed() = SignInClicked;
 }
--- client/lib/application/auth/sign_in_form/sign_in_form_state.dart
+++ client/lib/application/auth/sign_in_form/sign_in_form_state.dart
@@ -3,18 +3,22 @@ part of 'sign_in_form_bloc.dart';
 @freezed
 abstract class SignInFormState with _$SignInFormState {
   const factory SignInFormState({
+    required Name name,
     required EmailAddress emailAddress,
     required Password password,
+    required Role role,
     required bool isSubmitting,
     required bool showErrorMessages,
     required Option<Either<AuthFailure, User>> authFailureOrSuccess,
   }) = _SignInFormState;
 
   factory SignInFormState.initial() => SignInFormState(
-    emailAddress: EmailAddress(""), 
-    password: Password(""), 
-    isSubmitting: false, 
-    showErrorMessages: false, 
-    authFailureOrSuccess: none(),
-  );
+        name: Name(""),
+        emailAddress: EmailAddress(""),
+        password: Password(""),
+        role: Role(""),
+        isSubmitting: false,
+        showErrorMessages: false,
+        authFailureOrSuccess: none(),
+      );
 }
