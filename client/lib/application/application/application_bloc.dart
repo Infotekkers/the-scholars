d0aab28e8fe98a6b49fe618613b3ea8e74d94bad
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2021-09-04 18:15:59 +0300

    Implemented Application bloc

--- client/lib/application/application/application_bloc.dart
+++ client/lib/application/application/application_bloc.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:client/domain/application/application.dart';
+import 'package:client/domain/application/application_failure.dart';
 import 'package:client/domain/application/i_application_repository.dart';
+import 'package:client/domain/application/value_objects.dart';
+import 'package:client/domain/core/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
@@ -19,5 +23,80 @@ class ApplicationBloc extends Bloc<ApplicationEvent, ApplicationState> {
   @override
   Stream<ApplicationState> mapEventToState(
     ApplicationEvent event,
-  ) async* {}
+  ) async* {
+    yield* event.map(
+      initialEvent: (e) async* {
+        // Return Inital State -- empty state
+        yield ApplicationState.initial();
+      },
+      schoolTranscriptChanged: (e) async* {
+        yield state.copyWith(
+          schoolTranscript:
+              SchoolTranscript(schoolTranscriptPath: e.schoolTranscriptPath),
+          showErrorMessages: true,
+          applicationFailureOrSuccess: none(),
+          isSubmitting: false,
+        );
+      },
+      reccomnedationLetterChanged: (e) async* {
+        yield state.copyWith(
+          recomendationLetter: RecomendationLetter(
+            recomendationLetterPath: e.reccomendationLetterPath,
+          ),
+          showErrorMessages: true,
+          applicationFailureOrSuccess: none(),
+          isSubmitting: false,
+        );
+      },
+      mainEssayChanged: (e) async* {
+        yield state.copyWith(
+          mainEssay: MainEssay(mainEssayPath: e.mainEssayPath),
+          showErrorMessages: true,
+          isSubmitting: false,
+        );
+      },
+      extraEssayChanged: (e) async* {
+        yield state.copyWith(
+          extraEssay: ExtraEssay(extraEssayPath: e.extraEssay),
+          showErrorMessages: true,
+          applicationFailureOrSuccess: none(),
+          isSubmitting: false,
+        );
+      },
+      militaryStatusChanged: (e) async* {
+        yield state.copyWith(
+          militaryFamilyStatus:
+              MilitaryFamilyStatus(militaryFamilyStatus: e.militaryStatus),
+          showErrorMessages: true,
+          isSubmitting: false,
+        );
+      },
+      universityStatusChanged: (e) async* {
+        yield state.copyWith(
+          universityFamilyStatus: UniversityFamilyStatus(
+              universityFamilyStatus: e.universityStatus),
+          isSubmitting: false,
+          showErrorMessages: true,
+        );
+      },
+      proficiencyUrlChanged: (e) async* {
+        yield state.copyWith(
+          proficencyTest: ProficencyTest(proficiencyUrl: e.testUrl),
+          showErrorMessages: true,
+          applicationFailureOrSuccess: none(),
+          isSubmitting: false,
+        );
+      },
+      extraCertificationChanged: (e) async* {
+        yield state.copyWith(
+          extraCertification: ExtraCertification(
+              extraCertificationPath: e.extraCertificationPath),
+          showErrorMessages: true,
+          applicationFailureOrSuccess: none(),
+          isSubmitting: false,
+        );
+      },
+      submitApplicationClicked: (e) async* {},
+    );
+  }
 }
