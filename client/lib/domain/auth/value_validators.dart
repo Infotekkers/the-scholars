5e70d3e8917837d9e7f485165dea1cb9ca986053
BarokDG <it.barok.dagim@gmail.com>
2021-09-05 20:22:14 +0300

    email validation check

--- client/lib/domain/auth/value_validators.dart
+++ client/lib/domain/auth/value_validators.dart
@@ -2,8 +2,15 @@ import 'package:client/domain/core/failures.dart';
 import 'package:dartz/dartz.dart';
 
 Either<ValueFailure<String>, String> validateEmailAddress(String emailStr) {
-  // TODO: Implement validation
-  return right("");
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+
+  if (RegExp(emailRegex).hasMatch(emailStr)) {
+    return right(emailStr);
+  } else {
+    return left(ValueFailure.invalidEmail(failedValue: emailStr));
+  }
+  
 }
 
 Either<ValueFailure<String>, String> validatePassword(String passwordStr) {
