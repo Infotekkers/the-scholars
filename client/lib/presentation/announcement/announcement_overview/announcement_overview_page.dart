f58cb3cd6530645dd95c494393310beef2ee3b1c
ermias asmare <it.ermias.asmare@gmail.com>
2021-09-11 01:37:51 +0300

    finished the announcement over page and overiew body

---
+++ client/lib/presentation/announcement/announcement_overview/announcement_overview_page.dart
@@ -0,0 +1,47 @@
+import 'package:client/application/announcements/announcement_actor/announcement_actor_bloc.dart';
+import 'package:client/application/announcements/announcement_watcher/announcement_watcher_bloc.dart';
+import 'package:client/injectable.dart';
+import 'package:client/presentation/announcement/announcement_overview/widgets/announcements_overview_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AnnouncementsOverviewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider<AnnouncementActorBloc>(
+              create: (context) => getIt<AnnouncementActorBloc>()),
+          BlocProvider<AnnouncementWatcherBloc>(
+              create: (context) => getIt<AnnouncementWatcherBloc>()),
+        ],
+        child: MultiBlocListener(listeners: [
+          BlocListener<AnnouncementActorBloc, AnnouncementActorState>(
+              listener: (context, state) {
+            state.maybeMap(
+                actionFailure: (state) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(state.announcementFailure.map(
+                          unexpected: (_) => "Unexpected error",
+                          serverError: (_) => "Server error",
+                          networkError: (_) => "No connection",
+                          cancelledByUser: (_) => "Cancelled by user"))));
+                },
+                orElse: () {});
+          }),
+          BlocListener<AnnouncementWatcherBloc, AnnouncementWatcherState>(
+              listener: (context, state) {
+            state.maybeMap(
+                loadFailure: (state) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(state.announcementFailure.map(
+                          unexpected: (_) => "Unexpected error",
+                          serverError: (_) => "Server error",
+                          networkError: (_) => "No connection",
+                          cancelledByUser: (_) => "Cancelled by user"))));
+                },
+                orElse: () {});
+          }),
+        ], child: AnnouncementsOverviewBody()));
+  }
+}
