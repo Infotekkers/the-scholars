71ecf00a4f938402b8a6b37d81cefddbc49c2d22
michael alemayehu <mikealexiv565@gmail.com>
2021-09-11 02:28:14 +0300

    injectable fix

--- client/lib/injectable.config.dart
+++ client/lib/injectable.config.dart
@@ -23,13 +23,13 @@ import 'application/auth/sign_in_form/sign_in_form_bloc.dart' as _i13;
 import 'application/navigation/navigation_bloc.dart' as _i11;
 import 'application/util/util_bloc.dart' as _i14;
 import 'application/view_application/view_application_bloc.dart' as _i15;
-import 'domain/announcement/i_admin_announcement_repository.dart' as _i3;
-import 'domain/application/i_admin_application_repository.dart' as _i5;
+import 'domain/announcement/i_admin_announcement_repository.dart' as _i5;
+import 'domain/application/i_admin_application_repository.dart' as _i3;
 import 'domain/application/i_application_repository.dart' as _i7;
 import 'domain/auth/i_auth_repository.dart' as _i9;
-import 'infrastructure/announcement/announcement_admin_repository.dart' as _i4;
+import 'infrastructure/announcement/announcement_admin_repository.dart' as _i6;
 import 'infrastructure/application/api_admin_application_repository.dart'
-    as _i6;
+    as _i4;
 import 'infrastructure/application/api_application_repository.dart' as _i8;
 import 'infrastructure/auth/api_auth_repository.dart'
     as _i10; // ignore_for_file: unnecessary_lambdas
@@ -39,10 +39,10 @@ import 'infrastructure/auth/api_auth_repository.dart'
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.lazySingleton<_i3.IAdminAnnouncementRepository>(
-      () => _i4.AnnoncementAdminRepository());
-  gh.lazySingleton<_i5.IAdminApplicationRepository>(
-      () => _i6.ApiAdminApplicationRepository());
+  gh.lazySingleton<_i3.IAdminApplicationRepository>(
+      () => _i4.ApiAdminApplicationRepository());
+  gh.lazySingleton<_i5.IAnnouncementRepository>(
+      () => _i6.AnnoncementRepository());
   gh.lazySingleton<_i7.IApplicationRepository>(
       () => _i8.ApiApplicationRepository());
   gh.lazySingleton<_i9.IAuthRepository>(() => _i10.ApiAuthRepository());
@@ -53,19 +53,19 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
       () => _i13.SignInFormBloc(get<_i9.IAuthRepository>()));
   gh.lazySingleton<_i14.UtilBloc>(() => _i14.UtilBloc());
   gh.lazySingleton<_i15.ViewApplicationBloc>(() => _i15.ViewApplicationBloc());
-  gh.factory<_i16.AnnouncementActorBloc>(() =>
-      _i16.AnnouncementActorBloc(get<_i3.IAdminAnnouncementRepository>()));
+  gh.factory<_i16.AnnouncementActorBloc>(
+      () => _i16.AnnouncementActorBloc(get<_i5.IAnnouncementRepository>()));
   gh.factory<_i17.AnnouncementFormBloc>(
-      () => _i17.AnnouncementFormBloc(get<_i3.IAdminAnnouncementRepository>()));
-  gh.factory<_i18.AnnouncementWatcherBloc>(() =>
-      _i18.AnnouncementWatcherBloc(get<_i3.IAdminAnnouncementRepository>()));
+      () => _i17.AnnouncementFormBloc(get<_i5.IAnnouncementRepository>()));
+  gh.factory<_i18.AnnouncementWatcherBloc>(
+      () => _i18.AnnouncementWatcherBloc(get<_i5.IAnnouncementRepository>()));
   gh.lazySingleton<_i19.ApplicationBloc>(
       () => _i19.ApplicationBloc(get<_i7.IApplicationRepository>()));
   gh.factory<_i20.ApplicationsOverviewActorBloc>(() =>
       _i20.ApplicationsOverviewActorBloc(
-          get<_i5.IAdminApplicationRepository>()));
+          get<_i3.IAdminApplicationRepository>()));
   gh.factory<_i21.ApplicationsOverviewWatcherBloc>(() =>
       _i21.ApplicationsOverviewWatcherBloc(
-          get<_i5.IAdminApplicationRepository>()));
+          get<_i3.IAdminApplicationRepository>()));
   return get;
 }
