0c1e2319bb6bf08414acff9d590b715ebbe2a6ac
ermias asmare <it.ermias.asmare@gmail.com>
2021-09-11 18:12:05 +0300

    updated ui and authorization

--- client/lib/presentation/auth/register_page.dart
+++ client/lib/presentation/auth/register_page.dart
@@ -8,9 +8,7 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register Here'),
-      ),
+     
       body: BlocProvider(
         create: (BuildContext context) => getIt<RegisterFormBloc>(),
         child: RegisterForm(),
