d0faa07d980e67bbdda681944d8edcf86ca352bc
ermias asmare <it.ermias.asmare@gmail.com>
2021-09-11 04:24:45 +0300

    updated the ui for the announcement posting form

--- client/lib/presentation/announcement/announcement_form/widgets/announcement_form.dart
+++ client/lib/presentation/announcement/announcement_form/widgets/announcement_form.dart
@@ -29,75 +29,108 @@ class AnnouncementForm extends StatelessWidget {
                 }));
       },
       builder: (context, state) {
-        return Form(
-          autovalidateMode: state.showErrorMessages
-              ? AutovalidateMode.always
-              : AutovalidateMode.disabled,
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(5),
-                child: TextFormField(
-                  keyboardType: TextInputType.multiline,
-                  decoration: const InputDecoration(
-                    prefix: Icon(Icons.info_outlined),
-                    labelText: 'Title',
-                    helperText: 'Title must not exceed 50 characters',
+        return Scaffold(
+           backgroundColor: Colors.grey[900],
+          body: SingleChildScrollView(
+            child: Column(
+              children: [ 
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Form(
+                    autovalidateMode: state.showErrorMessages
+                        ? AutovalidateMode.always
+                        : AutovalidateMode.disabled,
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(5),
+                          child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: const InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                                borderSide: BorderSide(
+                                  color: Colors.white, width: 1,
+                                )
+                              ),
+                              fillColor: Colors.white,
+                              prefix: Icon(Icons.info_outlined),
+                              labelText: 'Title',
+                              labelStyle: TextStyle(color: Colors.white),
+                              helperText: 'Title must not exceed 50 characters',
+                            ),
+                            autocorrect: false,
+                            onChanged: (value) =>
+                                BlocProvider.of<AnnouncementFormBloc>(context)
+                                    .add(AnnouncementFormEvent.titleChanged(value)),
+                            validator: (_) =>
+                                BlocProvider.of<AnnouncementFormBloc>(context)
+                                    .state
+                                    .title
+                                    .value
+                                    .fold(
+                                        (failure) => failure.maybeMap(
+                                            empty: (_) => 'Title cannot be Empty',
+                                            exceedingLength: (_) =>
+                                                'Exceeding Characters',
+                                            multiline: (_) => 'Single Line Titles',
+                                            orElse: () => null),
+                                        (_) => null),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(5),
+                          child: TextFormField(
+                            maxLines: 10,
+                            keyboardType: TextInputType.multiline,
+                            decoration: const InputDecoration(
+                               enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                                  borderSide: BorderSide(
+                                    color: Colors.grey, width: 1
+                                  )
+                                ),
+                                helperText: 'Body must not exceed 250 characters',
+                                labelText: 'Enter Your Text Here',
+                                labelStyle: TextStyle(color: Colors.white),
+                                ),
+                            onChanged: (value) =>
+                                BlocProvider.of<AnnouncementFormBloc>(context)
+                                    .add(AnnouncementFormEvent.bodyChanged(value)),
+                            validator: (_) =>
+                                BlocProvider.of<AnnouncementFormBloc>(context)
+                                    .state
+                                    .body
+                                    .value
+                                    .fold(
+                                        (failure) => failure.maybeMap(
+                                            empty: (_) => 'Body cannot be Empty',
+                                            exceedingLength: (_) =>
+                                                'Exceeding Characters Limit',
+                                            orElse: () => null),
+                                        (_) => null),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: state.isSaving
+                              ? null
+                              : () {
+                                  BlocProvider.of<AnnouncementFormBloc>(context)
+                                      .add(const AnnouncementFormEvent.saved());
+                                },
+                          child: const Text('Post'),
+                        ),
+                      ],
+                    ),
                   ),
-                  autocorrect: false,
-                  onChanged: (value) =>
-                      BlocProvider.of<AnnouncementFormBloc>(context)
-                          .add(AnnouncementFormEvent.titleChanged(value)),
-                  validator: (_) =>
-                      BlocProvider.of<AnnouncementFormBloc>(context)
-                          .state
-                          .title
-                          .value
-                          .fold(
-                              (failure) => failure.maybeMap(
-                                  empty: (_) => 'Title cannot be Empty',
-                                  exceedingLength: (_) =>
-                                      'Exceeding Characters',
-                                  multiline: (_) => 'Single Line Titles',
-                                  orElse: () => null),
-                              (_) => null),
+                  ],
+                   
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(5),
-                child: TextFormField(
-                  maxLines: 10,
-                  keyboardType: TextInputType.multiline,
-                  decoration: const InputDecoration(
-                      helperText: 'Body must not exceed 250 characters',
-                      labelText: 'Body'),
-                  onChanged: (value) =>
-                      BlocProvider.of<AnnouncementFormBloc>(context)
-                          .add(AnnouncementFormEvent.bodyChanged(value)),
-                  validator: (_) =>
-                      BlocProvider.of<AnnouncementFormBloc>(context)
-                          .state
-                          .body
-                          .value
-                          .fold(
-                              (failure) => failure.maybeMap(
-                                  empty: (_) => 'Body cannot be Empty',
-                                  exceedingLength: (_) =>
-                                      'Exceeding Characters Limit',
-                                  orElse: () => null),
-                              (_) => null),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: state.isSaving
-                    ? null
-                    : () {
-                        BlocProvider.of<AnnouncementFormBloc>(context)
-                            .add(const AnnouncementFormEvent.saved());
-                      },
-                child: const Text('Post'),
-              ),
-            ],
+              ),],
+             
+            ),
           ),
         );
       },
